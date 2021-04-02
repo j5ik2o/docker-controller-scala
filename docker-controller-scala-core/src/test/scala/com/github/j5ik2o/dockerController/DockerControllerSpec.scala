3b18a6723158cd3ae7a812c501f40a0c89fe8aab
Junichi Kato <j5ik2o@gmail.com>
2021-04-02 16:23:59 +0900

    refactor

--- docker-controller-scala-core/src/test/scala/com/github/j5ik2o/dockerController/DockerControllerSpec.scala
+++ docker-controller-scala-core/src/test/scala/com/github/j5ik2o/dockerController/DockerControllerSpec.scala
@@ -1,10 +1,10 @@
 package com.github.j5ik2o.dockerController
 
+import com.github.dockerjava.api.DockerClient
 import com.github.dockerjava.api.command.CreateContainerCmd
 import com.github.dockerjava.api.model.HostConfig.newHostConfig
 import com.github.dockerjava.api.model.{ ExposedPort, Ports }
-import com.github.dockerjava.core.{ DefaultDockerClientConfig, DockerClientImpl }
-import com.github.dockerjava.httpclient5.ApacheDockerHttpClient
+import com.github.dockerjava.core.{ DefaultDockerClientConfig, DockerClientBuilder }
 import org.scalatest.BeforeAndAfter
 import org.scalatest.freespec.AnyFreeSpec
 import scalaj.http.Http
@@ -12,24 +12,14 @@ import scalaj.http.Http
 import scala.concurrent.duration.Duration
 
 class DockerControllerSpec extends AnyFreeSpec with BeforeAndAfter {
-  val dockerHost = "192.168.99.107"
 
-  val config = DefaultDockerClientConfig.createDefaultConfigBuilder
-    .withDockerHost(s"tcp://$dockerHost:2376")
-    .withDockerCertPath(System.getProperty("user.home") + "/.docker/machine/machines/default")
-    .withDockerTlsVerify(true)
-    .build()
+  val dockerClientConfig: DefaultDockerClientConfig = DockerClientConfigBuilder.loadDockerClientConfig()
+  val dockerClient: DockerClient                    = DockerClientBuilder.getInstance(dockerClientConfig).build()
 
-  val httpClient = new ApacheDockerHttpClient.Builder()
-    .dockerHost(config.getDockerHost)
-    .sslConfig(config.getSSLConfig)
-    .maxConnections(100).build
+  val dockerHost: String = dockerClientConfig.getDockerHost.getHost
+  val hostPort: Int      = RandomPortUtil.temporaryServerPort()
 
-  val dockerClient = DockerClientImpl.getInstance(config, httpClient)
-
-  val hostPort = RandomPortUtil.temporaryServerPort()
-
-  val dockerController = new DockerController(dockerClient)(
+  val dockerController: DockerController = new DockerController(dockerClient)(
     imageName = "nginx",
     tag = Some("latest")
   ) {
