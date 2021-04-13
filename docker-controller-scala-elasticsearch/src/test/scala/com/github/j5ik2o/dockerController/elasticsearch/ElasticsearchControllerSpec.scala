0c7aeacfe03bd87847c65ec376207bce699d0fa4
Junichi Kato <j5ik2o@gmail.com>
2021-04-13 15:19:28 +0900

    revised test code

--- docker-controller-scala-elasticsearch/src/test/scala/com/github/j5ik2o/dockerController/elasticsearch/ElasticsearchControllerSpec.scala
+++ docker-controller-scala-elasticsearch/src/test/scala/com/github/j5ik2o/dockerController/elasticsearch/ElasticsearchControllerSpec.scala
@@ -30,7 +30,7 @@ class ElasticsearchControllerSpec extends AnyFreeSpec with DockerControllerSpecS
           dockerHost,
           hostPort1,
           (1 * testTimeFactor).seconds,
-          Some((1 * testTimeFactor).seconds)
+          Some((3 * testTimeFactor).seconds)
         )
       )
     )
