12ee3267e6f027af894f31f7c730b2ecb073acf0
Junichi Kato <j5ik2o@gmail.com>
2021-06-30 02:27:23 +0900

    feat: add support for flyway

---
+++ docker-controller-scala-flyway/src/main/scala/com/github/j5ik2o/dockerController/flyway/FlywaySpecSupport.scala
@@ -0,0 +1,66 @@
+package com.github.j5ik2o.dockerController.flyway
+
+import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.callback.Callback
+import org.flywaydb.core.internal.jdbc.DriverDataSource
+
+import javax.sql.DataSource
+import scala.jdk.CollectionConverters._
+
+final case class PlaceholderConfig(
+    placeholderReplacement: Boolean = false,
+    placeholders: Map[String, String] = Map.empty,
+    placeholderPrefix: Option[String] = None,
+    placeholderSuffix: Option[String] = None
+)
+
+final case class FlywayConfig(
+    locations: Seq[String],
+    callbacks: Seq[Callback] = Seq.empty,
+    placeholderConfig: Option[PlaceholderConfig] = None
+)
+
+final case class FlywayConfigWithDataSource(driverDataSource: DataSource, config: FlywayConfig)
+
+final case class FlywayContext(flyway: Flyway, config: FlywayConfigWithDataSource)
+
+trait FlywaySpecSupport {
+  protected def flywayDriverClassName: String
+  protected def flywayDbHost: String
+  protected def flywayDbHostPort: Int
+  protected def flywayDbName: String
+  protected def flywayDbUserName: String
+  protected def flywayDbPassword: String
+  protected def flywayJDBCUrl: String
+
+  private def createFlywayContext(flywayConfigWithDataSource: FlywayConfigWithDataSource): FlywayContext = {
+    val configure = Flyway.configure()
+    configure.dataSource(flywayConfigWithDataSource.driverDataSource)
+    configure.locations(flywayConfigWithDataSource.config.locations: _*)
+    configure.callbacks(flywayConfigWithDataSource.config.callbacks: _*)
+    flywayConfigWithDataSource.config.placeholderConfig.foreach { pc =>
+      configure.placeholderReplacement(pc.placeholderReplacement)
+      configure.placeholders(pc.placeholders.asJava)
+      pc.placeholderPrefix.foreach { pp =>
+        configure.placeholderPrefix(pp)
+      }
+      pc.placeholderSuffix.foreach { ps =>
+        configure.placeholderSuffix(ps)
+      }
+    }
+    FlywayContext(configure.load(), flywayConfigWithDataSource)
+  }
+
+  private def createFlywayDataSource: DataSource = new DriverDataSource(
+    getClass.getClassLoader,
+    flywayDriverClassName,
+    flywayJDBCUrl,
+    flywayDbUserName,
+    flywayDbPassword
+  )
+  // s"jdbc:mysql://$flywayDbHost:$flywayDbHostPort/$flywayDbName?useSSL=false&user=$flywayDbUserName&password=$flywayDbPassword",
+
+  protected def createFlywayContext(flywayConfig: FlywayConfig): FlywayContext = createFlywayContext(
+    FlywayConfigWithDataSource(createFlywayDataSource, flywayConfig)
+  )
+}
--- docker-controller-scala-mysql/src/test/scala/com/github/j5ik2o/dockerController/MySQLControllerSpec.scala
+++ docker-controller-scala-mysql/src/test/scala/com/github/j5ik2o/dockerController/MySQLControllerSpec.scala
@@ -1,5 +1,6 @@
 package com.github.j5ik2o.dockerController
 
+import com.github.j5ik2o.dockerController.flyway.{ FlywayConfig, FlywaySpecSupport }
 import com.github.j5ik2o.dockerController.mysql.MySQLController
 import org.scalatest.freespec.AnyFreeSpec
 
@@ -7,12 +8,23 @@ import java.sql.{ Connection, DriverManager, ResultSet, Statement }
 import scala.concurrent.duration.{ Duration, DurationInt }
 import scala.util.control.NonFatal
 
-class MySQLControllerSpec extends AnyFreeSpec with DockerControllerSpecSupport {
+class MySQLControllerSpec extends AnyFreeSpec with DockerControllerSpecSupport with FlywaySpecSupport {
   val testTimeFactor: Int = sys.env.getOrElse("TEST_TIME_FACTOR", "1").toInt
   logger.debug(s"testTimeFactor = $testTimeFactor")
 
-  val hostPort: Int                                                  = RandomPortUtil.temporaryServerPort()
-  val rootPassword: String                                           = "test"
+  val hostPort: Int        = RandomPortUtil.temporaryServerPort()
+  val rootPassword: String = "test"
+
+  override protected def flywayDriverClassName: String = classOf[com.mysql.cj.jdbc.Driver].getName
+  override protected def flywayDbHost: String          = dockerHost
+  override protected def flywayDbHostPort: Int         = hostPort
+  override protected def flywayDbName: String          = "test"
+  override protected def flywayDbUserName: String      = "root"
+  override protected def flywayDbPassword: String      = rootPassword
+
+  override protected def flywayJDBCUrl: String =
+    s"jdbc:mysql://$flywayDbHost:$flywayDbHostPort/$flywayDbName?useSSL=false&user=$flywayDbUserName&password=$flywayDbPassword"
+
   val controller: MySQLController                                    = MySQLController(dockerClient)(hostPort, rootPassword, databaseName = Some("test"))
   override protected val dockerControllers: Vector[DockerController] = Vector(controller)
 
@@ -35,14 +47,17 @@ class MySQLControllerSpec extends AnyFreeSpec with DockerControllerSpecSupport {
       var stmt: Statement      = null
       var resultSet: ResultSet = null
       try {
-        Class.forName("com.mysql.cj.jdbc.Driver")
-        conn = DriverManager.getConnection(
-          s"jdbc:mysql://$dockerHost:$hostPort/test?user=root&password=$rootPassword"
-        )
+        Class.forName(flywayDriverClassName)
+        conn = DriverManager.getConnection(flywayJDBCUrl)
         stmt = conn.createStatement
-        resultSet = stmt.executeQuery("SELECT 1 FROM DUAL")
-        while (resultSet.next())
-          assert(resultSet.getInt(1) == 1)
+        val result = stmt.executeUpdate("INSERT INTO users VALUES(1, 'kato')")
+        assert(result == 1)
+        resultSet = stmt.executeQuery("SELECT * FROM users")
+        while (resultSet.next()) {
+          val id   = resultSet.getInt("id")
+          val name = resultSet.getString("name")
+          println(s"id = $id, name = $name")
+        }
       } catch {
         case NonFatal(ex) =>
           ex.printStackTrace()
@@ -57,4 +72,10 @@ class MySQLControllerSpec extends AnyFreeSpec with DockerControllerSpecSupport {
       }
     }
   }
+
+  override protected def afterStartContainers(): Unit = {
+    val flywayContext = createFlywayContext(FlywayConfig(Seq("flyway")))
+    flywayContext.flyway.migrate()
+  }
+
 }
