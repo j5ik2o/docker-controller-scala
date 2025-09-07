62a8a5e25a3e3c707bbe621d456e9d98be19fe62
Junichi Kato <j5ik2o@gmail.com>
2025-09-07 09:28:20 +0900

    fix(deps): resolve Elasticsearch client compatibility issue
    - Replace deprecated elasticsearch-rest-high-level-client with elasticsearch-rest-client v8.17.2
    - Fix NoSuchMethodError by using compatible versions of elasticsearch-java and rest-client
    - Remove unused imports from ElasticsearchControllerSpec

--- docker-controller-scala-elasticsearch/src/test/scala/com/github/j5ik2o/dockerController/elasticsearch/ElasticsearchControllerSpec.scala
+++ docker-controller-scala-elasticsearch/src/test/scala/com/github/j5ik2o/dockerController/elasticsearch/ElasticsearchControllerSpec.scala
@@ -4,17 +4,9 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient
 import co.elastic.clients.json.jackson.JacksonJsonpMapper
 import co.elastic.clients.transport.rest_client.RestClientTransport
 import com.github.j5ik2o.dockerController.{ DockerController, DockerControllerSpecSupport, WaitPredicates }
-import org.elasticsearch.client.{
-  RequestOptions,
-  RestClient,
-  RestClientBuilder,
-  RestHighLevelClient,
-  RestHighLevelClientBuilder
-}
+import org.elasticsearch.client.RestClient
 import org.scalatest.freespec.AnyFreeSpec
 import org.apache.http.HttpHost
-import org.apache.http.client.methods.RequestBuilder.options
-import org.elasticsearch.client.core.MainRequest
 
 import scala.concurrent.duration.{ Duration, DurationInt }
 import scala.util.control.NonFatal
